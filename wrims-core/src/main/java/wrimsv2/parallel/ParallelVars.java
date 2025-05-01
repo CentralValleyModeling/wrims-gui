e61759aa5a736d919a11a702fc7608bdcb4f357f
hxiedwr <hxie@water.ca.gov>
2025-05-01 11:32:22 -0700

    remove wrims-core

--- wrims-core/src/main/java/wrimsv2/parallel/ParallelVars.java
+++
@@ -1,8 +0,0 @@
-package wrimsv2.parallel;
-
-public class ParallelVars {
-	public int dataDay;
-	public int dataMonth;
-	public int dataYear;
-	public int timeArrayIndex;
-}
--- wrims-core/src/main/java/wrimsv2/solver/InitialClpSolver.java
+++
@@ -1,7 +0,0 @@
-package wrimsv2.solver;
-
-public class InitialClpSolver {
-	public InitialClpSolver(){
-		System.loadLibrary("jClp");
-	}
-}
