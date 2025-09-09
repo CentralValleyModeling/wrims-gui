dab95cf46b5b2f756f6637738bc8aadaf52afd5a
jmdegeorge <josh@rmanet.com>
2025-09-09 16:27:57 -0700

    issue #6 milestone 2 - eclipse dependencies for additional modules dwr-hecdssvue, jdiagram, xtext-editor, and xtext-editor-ui

--- dwr-hecdssvue/src/main/java/gov/ca/dwr/hecdssvue/actions/ReportButtonAction.java
+++ dwr-hecdssvue/src/main/java/gov/ca/dwr/hecdssvue/actions/ReportButtonAction.java
@@ -2,28 +2,13 @@ package gov.ca.dwr.hecdssvue.actions;
 
 import gov.ca.dwr.hecdssvue.DssPluginCore;
 import gov.ca.dwr.hecdssvue.components.ClearAllCheckBox;
-import gov.ca.dwr.hecdssvue.views.DSSMonthlyView;
-import gov.ca.dwr.hecdssvue.views.DSSPlotView;
-import gov.ca.dwr.hecdssvue.views.DSSTableView;
-import gov.ca.dwr.hecdssvue.views.DeliveryShortagesView;
-import gov.ca.dwr.hecdssvue.views.SanJoaquinRiverView;
-import gov.ca.dwr.hecdssvue.views.StorageFlowsView;
-import gov.ca.dwr.hecdssvue.views.WaterManagementActionsView;
+
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-
-import org.eclipse.ui.IWorkbench;
-import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PlatformUI;
-
-import wrimsv2_plugin.debugger.exception.WPPException;
-
-
 
 public class ReportButtonAction implements ActionListener {
 
@@ -37,5 +22,4 @@ public class ReportButtonAction implements ActionListener {
 			new ClearAllCheckBox();
 		}
 	}
-
 }
