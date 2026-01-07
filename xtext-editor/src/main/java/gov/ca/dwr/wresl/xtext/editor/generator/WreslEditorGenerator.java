4bb106f4321b72ec6e13e7c5ffe5c0476022084b
Zack Olson <zack@rmanet.com>
2026-01-07 13:37:54 -0800

    Reverted XText package renames for legacy grammar support. (#88)

---
+++ xtext-editor/src/main/java/gov/ca/dwr/wresl/xtext/editor/generator/WreslEditorGenerator.java
@@ -0,0 +1,18 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package gov.ca.dwr.wresl.xtext.editor.generator;
+
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.generator.IFileSystemAccess;
+import org.eclipse.xtext.generator.IGenerator;
+
+public class WreslEditorGenerator implements IGenerator {
+	public WreslEditorGenerator() {
+	}
+
+	public void doGenerate(Resource resource, IFileSystemAccess fsa) {
+	}
+}
